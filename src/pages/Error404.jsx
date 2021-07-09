54414e0bb1d290cd8786d2f454ca413c717e6551
Delile Sebastien <sebastiendelile@gmail.com>
2021-07-09 17:34:03 +0200

    feat: add render and CSS Error404
    add rendering for page Error404
    add corresponding CSS

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,8 +1,19 @@
 import { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import '../styles/Error404.scss';
 class Error404 extends Component {
   render() {
-    return <div>You're on the Error404 page</div>;
+    return (
+      <div className="error404">
+        <h2 className="error404__number">404</h2>
+        <p className="error404__message">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <p className="error404__link">
+          <Link to="/">Retourner sur la page d’accueil</Link>
+        </p>
+      </div>
+    );
   }
 }
 
