a1118849becc99853f6012999ca790c27167fec1
Delile Sebastien <sebastiendelile@gmail.com>
2021-07-21 19:35:28 +0200

    fix: Gallery reusable component
    Gallery component is not reusable because data is always the same
    add a props adverts in Gallery, and the data in Home
    improve the semantic and accessibility of Gallery and Home

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,13 +1,13 @@
 import { Component } from 'react';
 import Thumbnail from '../Thumbnail/Thumbnail';
 import './Gallery.scss';
-import advertSample from '../../data/adverts-sample.json';
 
 /**
  * Render the Gallery component.
  * To be used with Thumbnail children components inside.
  * @extends Component
- * @param {object} props - no props here
+ * @param {object} props
+ * @param {array} props.adverts - the list of adverts to display
  * @property {object} state - the state of the component
  * @property {array} state.adverts - a list of flat advertisements, will be obtained from a fetch
  */
@@ -21,7 +21,7 @@ class Gallery extends Component {
    * get the Flat list at the initialization of the component and put it in the state
    */
   componentDidMount() {
-    this.setState({ adverts: advertSample.adverts });
+    this.setState({ adverts: this.props.adverts });
   }
 
   /**
@@ -29,18 +29,21 @@ class Gallery extends Component {
    * @returns {Reactnode} jsx to be injected in the html
    */
   render() {
-    return (
-      <div className="gallery">
-        {this.state.adverts.map((advert) => (
-          <Thumbnail
-            key={advert.id}
-            title={advert.title}
-            cover={advert.cover}
-            id={advert.id}
-          />
-        ))}
-      </div>
-    );
+    if (Object.keys(this.state.adverts).length === 0) return null;
+    else
+      return (
+        <section className="gallery">
+          <h2 className="sr-only">Gallery de locations</h2>
+          {this.state.adverts.map((advert) => (
+            <Thumbnail
+              key={advert.id}
+              title={advert.title}
+              cover={advert.cover}
+              id={advert.id}
+            />
+          ))}
+        </section>
+      );
   }
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../../components/Hero/Hero';
 import Gallery from '../../components/Gallery/Gallery';
 import './Home.scss';
 import heroImage from '../../assets/home-hero.png';
+import advertSample from '../../data/adverts-sample.json';
 
 /**
  * Render the Home page
@@ -17,12 +18,13 @@ class Home extends Component {
   render() {
     return (
       <main className="home container">
+        <h1 className="sr-only">Page d'accueil</h1>
         <Hero
           title="Chez vous, partout et ailleurs"
           image={heroImage}
           isFixedHeight={false}
         />
-        <Gallery />
+        <Gallery adverts={advertSample.adverts} />
       </main>
     );
   }
